d21e2d9cde797e127f44250f92f6116878173017
YellOwStAr <34637887+hx-yellowstar@users.noreply.github.com>
2018-03-04 20:31:45 +0800

    add comments

--- blogsite/lab/labviews.py
+++ blogsite/lab/labviews.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 此项功能仍在开发中，目前只能看到一个世界地图（在lab/页面，没有链接可以直接到达，地图来自highcharts）
 
 def worldmap(request):
     return render(request, 'lab.html')
